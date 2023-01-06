7a61333cbbc00f5c92125d4a8f186a01eb985c02
Nigel Palmer <nigel.palmer@oxfordcc.co.uk>
2023-01-06 07:41:40 +0000

    gh-171: Query Builder field type mapping management

---
+++ grails-app/services/uk/ac/ox/softeng/maurodatamapper/plugins/explorer/querybuilder/QueryBuilderPrimitiveTypeProfileProviderService.groovy
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2020-2022 University of Oxford and Health and Social Care Information Centre, also known as NHS Digital
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+package uk.ac.ox.softeng.maurodatamapper.plugins.explorer.querybuilder
+
+import uk.ac.ox.softeng.maurodatamapper.profile.provider.JsonProfileProviderService
+
+import groovy.util.logging.Slf4j
+
+@Slf4j
+class QueryBuilderPrimitiveTypeProfileProviderService extends JsonProfileProviderService {
+
+    @Override
+    String getMetadataNamespace() {
+        'uk.ac.ox.softeng.maurodatamapper.plugins.explorer.querybuilder'
+    }
+
+    @Override
+    String getDisplayName() {
+        'Mauro Data Explorer - Query Builder Data Type'
+    }
+
+    @Override
+    String getVersion() {
+        '1.0.0'
+    }
+
+    @Override
+    String getJsonResourceFile() {
+        return 'queryBuilderPrimitiveTypeProfile.json'
+    }
+
+    @Override
+    List<String> profileApplicableForDomains() {
+        return ['PrimitiveType']
+    }
+}
