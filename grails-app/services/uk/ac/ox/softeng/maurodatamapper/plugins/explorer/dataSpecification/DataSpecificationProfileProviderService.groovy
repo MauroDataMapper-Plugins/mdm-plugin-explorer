4684f801758bdd4ee54d78e083443d6521bc93cb
Andrew Wilson <Andrew.Wilson@oxfordcc.co.uk>
2023-03-30 10:35:23 +0100

    gh-27 - create data specification profile

---
+++ grails-app/services/uk/ac/ox/softeng/maurodatamapper/plugins/explorer/dataSpecification/DataSpecificationProfileProviderService.groovy
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2020-2023 University of Oxford and NHS England
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+package uk.ac.ox.softeng.maurodatamapper.plugins.explorer.dataSpecification
+
+import uk.ac.ox.softeng.maurodatamapper.profile.provider.JsonProfileProviderService
+
+import groovy.util.logging.Slf4j
+
+@Slf4j
+class DataSpecificationProviderService extends JsonProfileProviderService {
+
+    @Override
+    String getMetadataNamespace() {
+        'uk.ac.ox.softeng.maurodatamapper.plugins.explorer.dataSpecification'
+    }
+
+    @Override
+    String getDisplayName() {
+        'Mauro Data Explorer - Data Specification Profile'
+    }
+
+    @Override
+    String getVersion() {
+        '1.0.0'
+    }
+
+    @Override
+    String getJsonResourceFile() {
+        return 'dataSpecificationProfile.json'
+    }
+
+    @Override
+    List<String> profileApplicableForDomains() {
+        return ['DataModel']
+    }
+}
